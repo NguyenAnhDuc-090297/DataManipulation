a208a99f3a28753f1ec34f9dc4b5446170cf3919
Duc Nguyen <090297anhduc@gmail.com>
2023-01-31 10:33:02 +0700

    mock data

--- src/test/java/com/lifesup/jpadata/JpaDataApplicationTests.java
+++ src/test/java/com/lifesup/jpadata/JpaDataApplicationTests.java
@@ -1,7 +1,14 @@
 package com.lifesup.jpadata;
 
+import com.lifesup.jpadata.model.Employee;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class JpaDataApplicationTests {
@@ -10,4 +17,20 @@ class JpaDataApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void testMockData() {
+        Employee emp = new Employee();
+        emp.setAge(25);
+        emp.setName("Hong");
+
+        // mock data
+        EntityManager entityManager = mock(EntityManager.class);
+        when(entityManager.find(Employee.class, 1L)).thenReturn(emp);
+
+//        Employee empData = (Employee) entityManager.createQuery("FROM Employee e WHERE e.id = :id")
+//                .setParameter("id", 1L)
+//                .getSingleResult();
+        Employee empData = entityManager.find(Employee.class, 1L);
+        assertEquals("Hong", empData.getName());
+    }
 }
